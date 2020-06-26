eae70be451f077303365e6233a513d04ecd7c5fa
Bhavitavaya Prakash Shrivastava <42152956+bhavitavayashrivastava@users.noreply.github.com>
2020-06-26 08:34:33 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+
+
+def main():
+    """ A Simple Iris EDA App"""
+
+    st.title("Iris EDA App with streamlit")
+
+    st.header("Streamlit is cool")
+    st.subheader("Series of Streamlit App is coming")
+
+
+if __name__ == '__main__':
+    main()
